1ed759d564b2d63931474bdac32e061bb78134c8
unknown <asotjrs@gmail.com>
2020-06-06 15:51:18 +0200

    adding the middleware

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express =require('express') ;
 const app=express();
 app.get('/',(req,res)=>{
 
+    app.use((req,res,next)=>{
+       console.log("here it'll block if we wouldn't call the next function");
+       next();
+    });
+
     const user={
         name:'abdelhadi',
         password:'secret'
